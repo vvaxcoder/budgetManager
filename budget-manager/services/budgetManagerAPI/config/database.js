d28b065a059d027a20e0a8707ee85229478f842f
voronkovvaleriy <voronglass84@gmail.com>
2018-08-19 10:09:49 +0300

    Add /config/database.js

---
+++ budget-manager/services/budgetManagerAPI/config/database.js
@@ -0,0 +1,24 @@
+/**
+ * сначала переключили mongoose на использование стандартного объекта Promise.
+ * Если этого не сделать, можно столкнуться с предупреждениями, выводимыми 
+ * в консоль. Затем мы создали стандартное подключение mongoose
+ * @param {*} mongoose 
+ * @param {*} config 
+ */
+module.exports = (mongoose, config) => {
+    const database = mongoose.connection;
+    mongoose.Promise = Promise;
+    mongoose.connet(config.database, {
+        useMongoClient: true,
+        promiseLabrary: global.Promise
+    });
+    database.on('error', error => console.log(`Connect to budgetManager database failed: ${error}`));
+    database.on('connected', () => console.log(`Connected to budgetManager database`));
+    database.on('disconected', () => console.log(`Dissconected from budgetManager database`));
+    process.on('SIGINT', () => {
+        database.close(() => {
+            console.log(`budgetManager terminated, connection closed`);
+            process.exit(0);
+        });
+    });
+};
