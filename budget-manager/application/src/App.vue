719e752d11358bef1ed35c2b2311443dd357a15a
Valerii Voronkov <voronglass84@gmail.com>
2018-11-13 16:48:10 +0300

    Add background-image and scalability for mobile
    Благодаря этому шагу мы обеспечим правильное отображение приложения на мобильных устройствах. Кроме того, на данном этапе работы мы загрузим иконки в стиле Material Design.

--- budget-manager/application/src/App.vue
+++ budget-manager/application/src/App.vue
@@ -10,13 +10,20 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  @import "./assets/styles";
+  body {
+    background: url('./assets/images/background.jpg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
 </style>
